39ae83bc65cf813d60fae6e15c7aadfd3b623c87
Przemek Burczyk <przemek.burczyk@gmail.com>
2015-05-05 00:31:46 +0200

    Added gauge example.

---
+++ Components/Gauge/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Threading;
+
+namespace Components.Gauge
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        private int score;
+        public int Score
+        {
+            get { return score; }
+            set { score = value; OnPropertyChanged(new PropertyChangedEventArgs("Score")); }
+        }
+
+        public ViewModel()
+        {
+            Score = 500;
+        }
+
+        private void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, args);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
