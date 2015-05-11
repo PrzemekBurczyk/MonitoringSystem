d58641ec75f77402b953a07e77194f099d882c43
Jan Resiak <jan.resiak@sigmapoint.co>
2015-05-11 22:17:34 +0200

    Console component added

---
+++ Components/Console/Console.xaml.cs
@@ -0,0 +1,67 @@
+﻿using GuiComponentInterfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Components.Console
+{
+    /// <summary>
+    /// Interaction logic for Console.xaml
+    /// </summary>
+    public partial class Console : UserControl, IGuiComponent
+    {
+        private ViewModel ViewModel { get; set; }
+
+        public void addData(string line)
+        {
+            ViewModel.ConsoleText += "\n" + line;
+        }
+
+        public void addData(string[] lines)
+        {
+            foreach (string line in lines) {
+                addData(line);
+            }
+        }
+
+        public void clearData()
+        {
+            ViewModel.ConsoleText = "";
+        }
+
+        public Console()
+        {
+            InitializeComponent();
+            Loaded += Console_Loaded;
+        }
+
+        private void Console_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModel = (ViewModel)MainGrid.DataContext;
+            ViewModel.OnConsoleTextChanged += new EventHandler(OnConsoleTextChanged);
+        }
+
+        private void OnConsoleTextChanged(object sender, EventArgs eventArgs)
+        {
+            textBox.ScrollToEnd();
+        }
+
+        public DataType[] GetTypes()
+        {
+            return new DataType[] { DataType.TEXT, DataType.VECTOR };
+        }
+
+    }
+}
---
+++ Components/Console/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Components.Console
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public EventHandler OnConsoleTextChanged;
+
+        private string consoleText;
+        public string ConsoleText
+        {
+            get { return consoleText; }
+            set
+            {
+                if (value != consoleText)
+                {
+                    consoleText = value;
+                    OnPropertyChanged("ConsoleText");
+                    if (OnConsoleTextChanged != null)
+                    {
+                        OnConsoleTextChanged(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string p)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(p));
+            }
+        }
+
+        public ViewModel()
+        {
+            ConsoleText = "";
+        }
+    }
+}
